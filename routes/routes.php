b976bea8d5b8d24246edecb1fb46ec22dbc2a920
Vital Ozierski <v.ozierski@ozitag.com>
2020-07-16 17:15:19 +0300

    change public endpoint to /tager/***

--- routes/routes.php
+++ routes/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/seo/{page}', \OZiTAG\Tager\Backend\Seo\Controllers\PublicController::class . '@page');
+Route::get('/tager/seo/{page}', \OZiTAG\Tager\Backend\Seo\Controllers\PublicController::class . '@page');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['passport:administrators', 'auth:api']], function () {
     Route::get('/seo/pages', \OZiTAG\Tager\Backend\Seo\Controllers\AdminController::class . '@index');
